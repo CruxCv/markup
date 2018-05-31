fd2a36139a22d5b3edcee464e9a76425f124810f
難十字星丶 <specialweiyu@gmail.com>
2018-05-31 17:44:14 +0800

    Update SelfAudio.js

--- component-common/SelfAudio/SelfAudio.js
+++ component-common/SelfAudio/SelfAudio.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./SelfAudio.scss";
+import "./SelfAudio.less";
 
 export class SelfAudio extends React.Component {
   constructor(props) {
